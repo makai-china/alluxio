bd58b736ea8ce074d6e1498a78762447686ed9ff
Xin Wu <492960551@qq.com>
2017-03-08 16:08:17 +0800

    FIx Format

--- underfs/qingstor/src/main/java/alluxio/underfs/qingstor/QingstorOutputStream.java
+++ underfs/qingstor/src/main/java/alluxio/underfs/qingstor/QingstorOutputStream.java
@@ -27,136 +27,134 @@ import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * A stream for writing a file into Qingstor. The data will be persisted to a temporary directory on the
- * local disk and copied as a complete file when the {@link #close()} method is called.
+ * A stream for writing a file into Qingstor. The data will be persisted to a temporary directory on
+ * the local disk and copied as a complete file when the {@link #close()} method is called.
  */
-public class QingstorOutputStream  extends OutputStream {
-    private static final Logger LOG = LoggerFactory.getLogger(QingstorOutputStream.class);
+public class QingstorOutputStream extends OutputStream {
+  private static final Logger LOG = LoggerFactory.getLogger(QingstorOutputStream.class);
 
-    /** The Bucket of Qingstor for object operations. */
-    private final Bucket mBucket;
+  /** The Bucket of Qingstor for object operations. */
+  private final Bucket mBucket;
 
-    /** Key of the file when it is uploaded to OSS. */
-    private final String mKey;
+  /** Key of the file when it is uploaded to OSS. */
+  private final String mKey;
 
-    /** The local file that will be uploaded when the stream is closed. */
-    private final File mFile;
+  /** The local file that will be uploaded when the stream is closed. */
+  private final File mFile;
 
-    /** The outputstream to a local file where the file will be buffered until closed. */
-    private OutputStream mLocalOutputStream;
+  /** The outputstream to a local file where the file will be buffered until closed. */
+  private OutputStream mLocalOutputStream;
 
-    /** The MD5 hash of the file. */
-    private MessageDigest mHash;
+  /** The MD5 hash of the file. */
+  private MessageDigest mHash;
 
-    /** Flag to indicate this stream has been closed, to ensure close is only done once. */
-    private AtomicBoolean mClosed = new AtomicBoolean(false);
+  /** Flag to indicate this stream has been closed, to ensure close is only done once. */
+  private AtomicBoolean mClosed = new AtomicBoolean(false);
 
-    /**
-     * Creates a name instance of {@link QingstorOutputStream}.
-     *
-     * @param key the key of the file
-     * @param bucket
-     * @throws IOException if an I/O error occurs
-     */
-    public QingstorOutputStream(Bucket bucket, String key)
-            throws IOException {
-        Preconditions.checkArgument(key != null && !key.isEmpty(),
-                "Qingstor path must not be null or empty.");
-        Preconditions.checkArgument(bucket != null,
-                "Qingstor bucket must not be null.");
+  /**
+   * Creates a name instance of {@link QingstorOutputStream}.
+   *
+   * @param key the key of the file
+   * @param bucket
+   * @throws IOException if an I/O error occurs
+   */
+  public QingstorOutputStream(Bucket bucket, String key) throws IOException {
+    Preconditions.checkArgument(key != null && !key.isEmpty(),
+        "Qingstor path must not be null or empty.");
+    Preconditions.checkArgument(bucket != null, "Qingstor bucket must not be null.");
 
-        this.mKey = key;
-        this.mBucket = bucket;
+    this.mKey = key;
+    this.mBucket = bucket;
 
-        mFile = new File(PathUtils.concatPath("/tmp", UUID.randomUUID()));
+    mFile = new File(PathUtils.concatPath("/tmp", UUID.randomUUID()));
 
-        try {
-            mHash = MessageDigest.getInstance("MD5");
-            mLocalOutputStream =
-                new BufferedOutputStream(new DigestOutputStream(new FileOutputStream(mFile), mHash));
-        } catch (NoSuchAlgorithmException e) {
-            LOG.warn("Algorithm not available for MD5 hash.", e);
-            mHash = null;
-            mLocalOutputStream = new BufferedOutputStream(new FileOutputStream(mFile));
-        }
+    try {
+      mHash = MessageDigest.getInstance("MD5");
+      mLocalOutputStream =
+          new BufferedOutputStream(new DigestOutputStream(new FileOutputStream(mFile), mHash));
+    } catch (NoSuchAlgorithmException e) {
+      LOG.warn("Algorithm not available for MD5 hash.", e);
+      mHash = null;
+      mLocalOutputStream = new BufferedOutputStream(new FileOutputStream(mFile));
     }
+  }
 
-    /**
-     * Writes the given bytes to this output stream. Before close, the bytes are all written to local
-     * file.
-     *
-     * @param b the bytes to write
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    public void write(int b) throws IOException {
-        mLocalOutputStream.write(b);
-    }
+  /**
+   * Writes the given bytes to this output stream. Before close, the bytes are all written to local
+   * file.
+   *
+   * @param b the bytes to write
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void write(int b) throws IOException {
+    mLocalOutputStream.write(b);
+  }
 
-    /**
-     * Writes the given byte array to this output stream. Before close, the bytes are all written to
-     * local file.
-     *
-     * @param b the byte array
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    public void write(byte[] b) throws IOException {
-        mLocalOutputStream.write(b, 0, b.length);
-    }
+  /**
+   * Writes the given byte array to this output stream. Before close, the bytes are all written to
+   * local file.
+   *
+   * @param b the byte array
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void write(byte[] b) throws IOException {
+    mLocalOutputStream.write(b, 0, b.length);
+  }
 
-    /**
-     * Writes the given number of bytes from the given byte array starting at the given offset to this
-     * output stream. Before close, the bytes are all written to local file.
-     *
-     * @param b the byte array
-     * @param off the start offset in the data
-     * @param len the number of bytes to write
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    public void write(byte[] b, int off, int len) throws IOException {
-        mLocalOutputStream.write(b, off, len);
-    }
+  /**
+   * Writes the given number of bytes from the given byte array starting at the given offset to this
+   * output stream. Before close, the bytes are all written to local file.
+   *
+   * @param b the byte array
+   * @param off the start offset in the data
+   * @param len the number of bytes to write
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException {
+    mLocalOutputStream.write(b, off, len);
+  }
 
-    /**
-     * Flushes this output stream and forces any buffered output bytes to be written out. Before
-     * close, the data are flushed to local file.
-     *
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    public void flush() throws IOException {
-        mLocalOutputStream.flush();
-    }
+  /**
+   * Flushes this output stream and forces any buffered output bytes to be written out. Before
+   * close, the data are flushed to local file.
+   *
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void flush() throws IOException {
+    mLocalOutputStream.flush();
+  }
 
-    /**
-     * Closes this output stream. When an output stream is closed, the local temporary file is
-     * uploaded to Qingstor. Once the file is uploaded, the temporary file is deleted.
-     *
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    public void close() throws IOException {
-        if (mClosed.getAndSet(true)) {
-            return;
-        }
-        mLocalOutputStream.close();
-        try {
-            Bucket.PutObjectInput input = new Bucket.PutObjectInput();
-            input.setBodyInputFile(mFile);
-            input.setContentLength(mFile.length());
-            if (mHash != null) {
-                byte[] hashBytes = mHash.digest();
-                input.setContentMD5(new String(Base64.encodeBase64(hashBytes)));
-            }
-            mBucket.putObject(mKey, input);
-            if (!mFile.delete()) {
-                LOG.error("Failed to delete temporary file @ {}", mFile.getPath());
-            }
-        } catch (QSException e) {
-            LOG.error("Failed to upload {}. Temporary file @ {}", mKey, mFile.getPath());
-            throw new IOException(e);
-        }
+  /**
+   * Closes this output stream. When an output stream is closed, the local temporary file is
+   * uploaded to Qingstor. Once the file is uploaded, the temporary file is deleted.
+   *
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  public void close() throws IOException {
+    if (mClosed.getAndSet(true)) {
+      return;
     }
+    mLocalOutputStream.close();
+    try {
+      Bucket.PutObjectInput input = new Bucket.PutObjectInput();
+      input.setBodyInputFile(mFile);
+      input.setContentLength(mFile.length());
+      if (mHash != null) {
+        byte[] hashBytes = mHash.digest();
+        input.setContentMD5(new String(Base64.encodeBase64(hashBytes)));
+      }
+      mBucket.putObject(mKey, input);
+      if (!mFile.delete()) {
+        LOG.error("Failed to delete temporary file @ {}", mFile.getPath());
+      }
+    } catch (QSException e) {
+      LOG.error("Failed to upload {}. Temporary file @ {}", mKey, mFile.getPath());
+      throw new IOException(e);
+    }
+  }
 }
